21144c5918ca9d4564037f8772cd6c7daf0eddd5
jinho1011 <gino9940@gmail.com>
2020-07-27 15:47:47 +0900

    Implement make and write date list

--- sheets_manager.py
+++ sheets_manager.py
@@ -2,9 +2,11 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from datetime import datetime, timedelta, date, timezone
+import datetime
+import calendar
 import pickle
 import os.path
+import pandas
 
 
 def get_sheets_service():
@@ -29,7 +31,7 @@ def get_sheets_service():
     return service.spreadsheets()
 
 
-def write_content(sheet, sheet_id, range, content):
+def write_date(sheet, sheet_id, range, content):
     body = {
         'values': [
             content
@@ -41,7 +43,20 @@ def write_content(sheet, sheet_id, range, content):
 
 
 def make_date_list():
-    return None
+    dt_index = pandas.date_range(start='20200101', end='20201231')
+    dt_list = dt_index.tolist()
+    res_list = []
+    last_list = ['01-31', '02-29', '03-31', '04-30', '05-31',
+                 '06-30', '07-31', '08-31', '09-30', '10-31', '11-30', '12-31']
+
+    for dt in dt_list:
+        res_list.append(dt.strftime("%m-%d"))
+
+    for res in res_list:
+        if res in last_list:
+            res_list.insert(res_list.index(res) + 1, '')
+
+    return res_list
 
 
 if __name__ == "__main__":
@@ -49,5 +64,8 @@ if __name__ == "__main__":
 
     date = make_date_list()
 
-    write_content(
+    for d in date:
+        print(d)
+
+    write_date(
         sheet, '11AYfo9oaU2zMiRqQKFj5_CrxG6JIp3MTTalwgPmI0UQ', '시트1!E1', date)
