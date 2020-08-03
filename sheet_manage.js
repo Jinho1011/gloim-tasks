46638ef2349a8b64a6db81ee33f390cf4eff2f5e
jinho1011 <gino9940@gmail.com>
2020-08-03 09:21:28 +0900

    Remove google script

--- sheet_manage.js
+++
@@ -1,7 +0,0 @@
-function getCellColor() {
-	var ss = SpreadsheetApp.getActiveSpreadsheet();
-	var sheet = ss.getSheets()[0];
-
-	var cell = sheet.getRange("B5");
-	sheet.appendRow([cell.getBackground()]);
-}
